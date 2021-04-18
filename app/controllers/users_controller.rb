b85a2b701634889bba9b7b940babc1ed3d52f35c
docty89 <docty89@gmail.com>
2021-04-18 02:49:25 +0000

    [bookers[Finish]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
   def ensure_correct_user
     user = User.find(params[:id])
     if user != current_user
-      redirect_to user_path(current_user.id)
+      redirect_to user_path(current_user)
     end
     
   end
